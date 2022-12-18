f621911fe81a1444bd6f107ab3372ca965ddd8f7
Nicholas Jumaoas <NLJumaoas@EricNetnnahsMBP.attlocal.net>
2022-12-18 02:13:04 -0800

    sadge

--- test.py
+++ test.py
@@ -1,1 +1,45 @@
-print("sadge")
+import pandas as pd
+import json
+from pandas import json_normalize
+import requests
+
+variables = {
+  'username': '444mike',
+  'type': 'ANIME'
+}
+
+url = 'https://graphql.anilist.co'
+
+query = '''
+query ($username: String, $type: MediaType) {
+MediaListCollection(userName: $username, type: $type) {
+    lists {
+    name
+    entries {
+        id
+        status
+        score(format: POINT_10)
+        progress
+        notes
+        repeat
+        media {
+        chapters
+        volumes
+        idMal
+        episodes
+        title { romaji }
+        }
+    }
+    name
+    isCustomList
+    isSplitCompletedList
+    status
+    }
+}
+}
+'''
+
+response = requests.post(url, json={'query': query, 'variables': variables})
+jsonData = response.json()
+completed_entries = jsonData['data']['MediaListCollection']['lists'][0]['entries']
+michael_df = json_normalize(completed_entries)
