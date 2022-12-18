aeb92f35b4dea821fb187cf24e9e279fef36da87
Nicholas Jumaoas <NLJumaoas@EricNetnnahsMBP.attlocal.net>
2022-12-18 03:19:56 -0800

    game go brrrrr

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("hello weebs")
+from produce_completed_df import produce_completed_df
+
+sample_df = produce_completed_df('444mike')
+print(sample_df)
---
+++ produce_completed_df.py
@@ -0,0 +1,59 @@
+def produce_completed_df(username):
+    """
+    Returns a DataFrame of the user's completed shows.
+    ---
+    Parameters:
+    username: the username of the desired profile (str)
+    """
+    import pandas as pd
+    import json
+    from pandas import json_normalize
+    import requests
+
+    variables = {
+        'username': username,
+        'type': 'ANIME'
+    }
+
+    url = 'https://graphql.anilist.co'
+
+    query = '''
+query ($username: String, $type: MediaType) {
+MediaListCollection(userName: $username, type: $type) {
+    lists {
+    name
+    entries {
+        id
+        status
+        score(format: POINT_10_DECIMAL)
+        progress
+        notes
+        repeat
+        media {
+        chapters
+        volumes
+        idMal
+        episodes
+        title { romaji }
+        }
+    }
+    name
+    isCustomList
+    isSplitCompletedList
+    status
+    }
+}
+}
+'''
+
+    # posts a request to the anilist graph ql
+    response = requests.post(url, json={'query': query, 'variables': variables})
+    jsonData = response.json()
+    # slams the shit into a dictionary based on type (0 = completed, 1 = planning, etc.)
+    completed_entries = jsonData['data']['MediaListCollection']['lists'][0]['entries']
+    df = json_normalize(completed_entries)
+
+    return df
+
+
+
